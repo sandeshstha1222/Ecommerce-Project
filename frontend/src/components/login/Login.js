3be1cbebdb4a7ace075ae9b58b70275960af2f2b
Rijan984 <rijan18shrestha@gmail.com>
2023-03-18 15:22:56 +0545

    onchange function

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const initialState = {
+    email: '',
+    pass: '',
+  };
+  const [form, setForm] = useState(initialState);
+  console.log(form);
+
+  const handleChange = (fname, value) => {
+    setForm({ ...form, [fname]: value });
+  };
   return (
     <div>
       <div className="login">
         <div className="input">
-          <input type="text" placeholder="Email" />
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            value={form.email}
+            onChange={(e) => handleChange(e.target.name, e.target.value)}
+          />
         </div>
         <div className="input">
-          <input type="password" placeholder="Password" />
+          <input
+            type="password"
+            placeholder="Password"
+            name="pass"
+            value={form.pass}
+            onChange={(e) => handleChange(e.target.name, e.target.value)}
+          />
         </div>
       </div>
     </div>
